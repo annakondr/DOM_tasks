94d920bb9f5aaeac7d5593f5a87a7ce1903113b6
annakondr <tyukondra@gmail.com>
2019-05-17 15:59:13 +0300

    calendar_changed

--- Calendar/main.js
+++ Calendar/main.js
@@ -1,42 +1,38 @@
-function createCalendar(id, year, month) {
-    const element = document.getElementById(id);
-    const date = new Date(year, month - 1);
-    const headOfTable = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-    let table = '<table><tr>';
-
-    for (let i=0; i < headOfTable.length; i++) {
-        table += `<th>${headOfTable[i]}</th>`
-    }
-    table += '</tr><tr>';
-
-    function getDay(weekDay) { // получить номер дня недели, от 0(пн) до 6(вс)
-        let day = weekDay.getDay();
-        if (day === 0) {
-            day = 7;
-        }
-        return day - 1;
-    }
-
-    for (let i = 0; i < getDay(date); i++) {
-        table += '<td></td>';
-    }
-
-    while (date.getMonth() === month - 1) {
-        table += `<td>${ date.getDate()}</td>`;
-        if (getDay(date) % 7 === 6) {
-            table += '</tr><tr>';
-        }
-        date.setDate(date.getDate() + 1);
-    }
-
-    if (getDay(date) !== 0) {
-        for (var i = getDay(date); i < 7; i++) {
-            table += '<td></td>';
-        }
-    }
-    table += '</tr></table>';
-
-    element.innerHTML = table;
+function getDaysInMonth (year, mon) {
+    return new Date(year, mon, 0).getDate();
 }
 
-createCalendar("calendar", 2012, 9);
+function createCalendar(id, year, month) {
+    const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+    const table = document.createElement('table');
+    const header = document.createElement('tr');
+    header.className = 'header';
+    for (const day of days) {
+        header.insertAdjacentHTML('beforeend', `<th>${day}</th>`)
+    }
+    table.append(header);
+
+    const date = new Date (year, month-1);
+    let firstDay = date.getDay();
+    firstDay = firstDay === 0 ? 6 : firstDay - 1;
+
+    let nextDayToAdd = 1 - firstDay;
+
+    const daysInMonth = getDaysInMonth(year, month);
+    while (nextDayToAdd <= daysInMonth) {
+        const week = document.createElement('tr');
+        for (let i = 0; i < 7; i++) {
+            const day = document.createElement('td');
+            if (nextDayToAdd > 0 && nextDayToAdd <= daysInMonth) {
+                day.innerHTML = nextDayToAdd;
+            }
+            nextDayToAdd++;
+            week.append(day);
+        }
+        table.append(week);
+    }
+    return document.querySelector(`#${id}`).append(table);
+}
+
+    createCalendar("calendar", 2019, 5);
+
